b8da7bec1d010a21931ae345a77d0bb1011d723a
Vladyslav Burzakovskyy <vladislav.burzakovskij@satoshilabs.com>
2023-02-01 23:48:09 +0100

    Add Poetry config, update Nix dependencies

--- shell.nix
+++ shell.nix
@@ -6,19 +6,20 @@ stdenv.mkDerivation {
     # nodejs
     # yarn
     # python310Packages.requests
+    python310Packages.poetry
     python310Packages.django_3
-    # python310Packages.dj-database-url
-    # python310Packages.django-environ
+    python310Packages.dj-database-url
+    python310Packages.django-environ
     # python310Packages.whitenoise
-    # python310Packages.djangorestframework
-    # python310Packages.djangorestframework-simplejwt
+    python310Packages.djangorestframework
+    python310Packages.djangorestframework-simplejwt
     # python310Packages.cryptography
     # python310Packages.setuptools
-    # python310Packages.django-cors-headers
+    python310Packages.django-cors-headers
     # python310Packages.pillow
     # python310Packages.pyjwt
     # python310Packages.intelhex
-    # python310Packages.psycopg2
+    python310Packages.psycopg2
     # python39Packages.captcha - add when merged to nixpkgs
   ];
 }
